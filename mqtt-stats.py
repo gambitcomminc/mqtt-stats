383649d8e94ed20e788084fd0a89ccd7f2adbe1a
Gambit Communications, Inc <support@gambitcomm.com>
2020-01-17 11:38:12 -0500

    Add files via upload

--- mqtt-stats.py
+++ mqtt-stats.py
@@ -16,9 +16,9 @@ import socket
 import time
 import logging
 import threading
-import multiprocessing
-import webbrowser
 import binascii
+import re
+import ssl
 
 # debug.setLogger(debug.Debug('all'))
 
@@ -152,7 +152,7 @@ class _UpdateThread(threading.Thread, _IdleObject):
 	def update_main(self):
 		while True:
 			# wake up every second for response
-			time.sleep (0.999)
+			time.sleep (0.99999)
 
 			if main.is_stopped:
 				break
@@ -187,7 +187,6 @@ class Topic():
 		self.topic = topic
 		self.number = number
 		self.count = 1;		# seen at least once
-		self.last_count = 0;	# updated every second
 		self.bytes = bytes
 		self.last_time = last_time
 		self.last_payload = last_payload
@@ -211,8 +210,11 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     if main.clear_stats:
 	main.messages_received = 0
-	main.topics = dict()
 	main.num_topics = 0
+	main.topics = dict()
+	main.changed = dict()		# a small subset of topics that is changed
+	main.last_changed = dict()	# the changed in the last iteration
+					# GUI needs this to 0 out some values
 	main.clear_stats = False
 	main.clear_store = True
 
@@ -228,26 +230,40 @@ def on_message(client, userdata, msg):
     	main.num_topics += 1
 	newtopic = Topic(main.num_topics, msg.topic, bytes, now, msg.payload, None)
 	main.topics[msg.topic] = newtopic
+	thistopic = newtopic
     else:
     	existingtopic = main.topics[msg.topic]
 	existingtopic.count += 1
 	existingtopic.bytes += bytes
 	existingtopic.last_time = now
 	existingtopic.last_payload = msg.payload
+	thistopic = existingtopic
 
+    if thistopic.number not in main.changed:
+	main.changed[thistopic.number] = msg.topic
     return
 
 
+# reconnect and re-subscribe
 def on_disconnect(client, userdata, rc):
 
     if rc != 0:
     	logging.error ("unexpected disconnect: " + str(rc))
 
-def subscriber_client(addr):
-	client = mqtt.Client()
+    # since we specified reconnect_delay_set, it will automatically reconnect
+    # could keep track of re-connects
+
+def subscriber_client():
+	client = mqtt.Client(main.client_id)
 	client.on_connect = on_connect
 	client.on_message = on_message
 	client.on_disconnect = on_disconnect
+	client.reconnect_delay_set(min_delay=10, max_delay=60)
+
+	if (main.is_tls):
+#		logging.debug ("cafile " + main.cafile)
+		client.tls_set(ca_certs=main.cafile, certfile=main.certfile, keyfile=main.keyfile, tls_version=ssl.PROTOCOL_SSLv23, cert_reqs=main.required)
+		client.tls_insecure_set(True)
 
 	client.connect(main.host_ip, main.port_num, 60)
 
@@ -260,8 +276,14 @@ class MyApp:
 		self.host_ip = None
 		self.port_num = None
 		self.verbose = False
+		self.client_id = None
 		self.topic = '#'
 		self.qos = 0
+		self.is_tls = False
+		self.cafile = ""
+		self.certfile = None
+		self.keyfile = None
+		self.required = ssl.CERT_NONE
 
 		self.is_stopped = False
 		self.is_paused = False
@@ -270,16 +292,31 @@ class MyApp:
 		self.last_received = 0
 		self.num_topics = 0
 		self.topics = dict()
+		self.changed = dict()
+		self.last_changed = dict()
 		self.clear_stats = False
 		self.clear_store = False
+		self.now = 0
+		self.last_time = 0
+
+		self.excluded = 0
+		self.shown = dict()		# topic numbers shown
+		self.matchprog = None		# regular expression for match
 
 	def usage(self):
 		print ("Usage: mqtt-stats.py")
-		print ("\t[-h|--host host]        broker to connect to; default localhost")
-		print ("\t[-p|--port port]        port to connect to; default port 1883")
-		print ("\t[-t|--topic topic]      topic; default #")
-		print ("\t[-q|--qos qos]          QoS; default 0")
-		print ("\t[-v|--verbose]    verbose output")
+		print ("\t[-h|--host host]   broker to connect to; default localhost")
+		print ("\t[-p|--port port]   port to connect to; default port 1883")
+		print ("\t[-i|--id clientid] client ID; default random")
+		print ("\t[-t|--topic topic] topic; default #")
+		print ("\t[-q|--qos qos]     QoS; default 0")
+		print ("\t[-v|--verbose]     verbose output")
+		print ("")
+		print ("\t[-T|--tls]         use TLS")
+		print ("\t[-c|--cafile]      certificate authority file for TLS")
+		print ("\t[-C|--certfile]    client certificate file for TLS")
+		print ("\t[-K|--keyfile]     client private key file for TLS")
+		print ("\t[-R|--required NONE|OPTIONAL|REQUIRED] certificate required")
 		return
 
 	def start(self):
@@ -290,7 +327,7 @@ class MyApp:
 		self.show_gui()
 		# from now on GUI is expected to be up
 
-		subscriber_client (self.host_ip)
+		subscriber_client ()
 
 		self.update_thread = _UpdateThread(self)
 		self.update_thread.connect("completed", self.completed_cb)
@@ -301,7 +338,7 @@ class MyApp:
 	###############################
 	def command_line(self):
 		try:
-			opts, args = getopt.getopt(sys.argv[1:], "h:p:t:q:v", ["host=", "port=", "topic=", "qos=", "verbose"])
+			opts, args = getopt.getopt(sys.argv[1:], "h:p:i:t:q:vTc:C:K:R:", ["host=", "port=", "id=", "topic=", "qos=", "verbose", "tls", "cafile=", "certfile=", "keyfile=", "required="])
 		except getopt.GetoptError as err:
 			# print help information and exit:
 			logging.error (str(err)) # will print something like "option -a not recognized"
@@ -315,10 +352,29 @@ class MyApp:
 				self.host_ip = a
 			elif o in ("-p", "--port"):
 				self.port_num = a
+			elif o in ("-i", "--id"):
+				self.client_id = a
 			elif o in ("-t", "--topic"):
 				self.topic = a
 			elif o in ("-q", "--qos"):
 				self.qos = int(a)
+			elif o in ("-T", "--tls"):
+				self.is_tls = True
+			elif o in ("-c", "--cafile"):
+				self.cafile = a
+			elif o in ("-C", "--certfile"):
+				self.certfile = a
+			elif o in ("-K", "--keyfile"):
+				self.keyfile = a
+			elif o in ("-R", "--required"):
+				if a == "NONE":
+					self.required = ssl.CERT_NONE
+				elif a == "OPTIONAL":
+					self.required = ssl.CERT_OPTIONAL
+				elif a == "REQUIRED":
+					self.required = ssl.CERT_REQUIRED
+				else:
+					assert False, "needs to be one of NONE, OPTIONAL, REQUIRED"
 			else:
 			    assert False, "unhandled option"
 
@@ -399,12 +455,18 @@ class MyApp:
 		self.infolabel6 = self.builder.get_object("infolabel6")
 		self.infolabel6.set_text("")
 
+		self.infolabel7 = self.builder.get_object("infolabel7")
+		self.infolabel7.set_text("")
+
 		# the liststore containing the agents
 		self.topicstore = self.builder.get_object("topicstore")
 
+		self.treefilter = self.builder.get_object("treemodelfilter1")
+		self.treefilter.set_visible_func(self.filter_func, data=None)
+
 		treeview = self.builder.get_object("treeview1")
 		self.treeview = treeview
-		tvcolumn = Gtk.TreeViewColumn('Number')
+		tvcolumn = Gtk.TreeViewColumn('No')
 		treeview.append_column(tvcolumn)
 		numbercell = Gtk.CellRendererText(xalign=1.0)
 		numbercell.set_property('ellipsize', pango.EllipsizeMode.END)
@@ -426,7 +488,7 @@ class MyApp:
 		#tvcolumn.set_fixed_width(80)
 		#tvcolumn.set_expand(True)
 
-		tvcolumn = Gtk.TreeViewColumn('Messages')
+		tvcolumn = Gtk.TreeViewColumn('Msgs')
 		treeview.append_column(tvcolumn)
 		tvcolumn.pack_start(numbercell, True)
 		tvcolumn.add_attribute(numbercell, 'text', 2)
@@ -463,12 +525,27 @@ class MyApp:
 
 		self.window.show_all()
 
+	@trace
+	def filter_func(self, model, iter, data):
+		if self.matchprog is None:
+			return True
+		if self.matchprog.search (model[iter][1]) is not None:
+			if model[iter][0] not in self.shown:
+				self.shown[model[iter][0]] = model[iter][0]
+			return True
+		self.excluded += 1
+		return False
+
 	###############################
 	# GUI component, could be separated out into a GUI class
 	# the callback on every poller cycle
 	@trace
 	def completed_cb(self, thread):
 		# logging.debug ("completed_cb - enter")
+
+		self.last_time = self.now
+		self.now = time.time()
+		self.excluded = 0
 
 		# according to https://en.wikibooks.org/wiki/GTK%2B_By_Example/Tree_View/Tree_Models#Speed_Issues_when_Adding_a_Lot_of_Rows
 		# while we update the model, detach it from the view
@@ -478,19 +555,31 @@ class MyApp:
 
 		if main.clear_store:
 			main.topicstore.clear()
+			main.shown.clear()
 			main.clear_store = False
 
-		msgpersec = self.messages_received - self.last_received
+		msgpersec = int ((self.messages_received - self.last_received) / (self.now - self.last_time))
 		self.last_received = self.messages_received
 		main.infolabel3.set_markup('<span foreground="blue">Messages received: ' + str(main.messages_received) + '</span>')
 		main.infolabel5.set_markup('<span foreground="blue">Messages / sec: ' + str(msgpersec) + '</span>')
 		main.infolabel4.set_markup('<span foreground="green">Topics: ' + str(len(main.topics)) + '</span>')
 
-		# run through the topics and add to the matrix
+		# run through the changed topics and add to the matrix
 		active_topics = 0
 		updated_entries = 0
-		keys = main.topics.keys()
-		for key in keys:
+
+		# take a snapshot of main.changed and main.last_changed
+		changed = main.changed
+		last_changed = main.last_changed
+		main.last_changed = main.changed
+		main.changed = dict()
+		numbers = set (changed.keys()) | set(last_changed.keys())
+		for number in numbers:
+		    key = changed.get(number, last_changed.get(number, None))
+		    if key == None:
+		    	logging.error ('number ' + str(number) + ' not in changed or last_changed')
+			continue
+
 		    topic =  main.topics[key]
 
 		    if topic.rowref == None:
@@ -514,14 +603,13 @@ class MyApp:
 			    # do for payload
 			    # happens much less frequently for topic
 
-			# TODO calculate msgpersec
-			msgpersec = topic.count - topic.last_count
+			msgpersec = topic.count / (self.now - self.last_time)
 
 			rowref = main.topicstore.append(
 				[
 				topic.number,
 				topicstr,
-				1,
+				topic.count,
 				msgpersec,
 				topic.bytes,
 				time.ctime (topic.last_time),
@@ -534,13 +622,11 @@ class MyApp:
 		    else:
 		    	# redisplay only if new messages for topic
 			displayedcount = main.topicstore.get_value (topic.rowref, 2)
-			# TODO calculate msgpersec
-			msgpersec = topic.count - topic.last_count
+			msgpersec = int((topic.count - displayedcount) / (self.now - self.last_time))
 			displayedmsgpersec = main.topicstore.get_value (topic.rowref, 3)
-			do_display = False
+			do_display = True
 			if topic.count != displayedcount:
 				active_topics += 1
-				do_display = True
 			else:
 				if msgpersec != displayedmsgpersec:
 					do_display = True
@@ -555,7 +641,6 @@ class MyApp:
 					# logging.debug ("UnicodeError: payload is not UTF-8 " + topic.last_payload + " >> " + payloadstr)
 				main.topicstore.set (topic.rowref, 2, topic.count, 3, msgpersec, 4, topic.bytes, 5, time.ctime (topic.last_time), 6, payloadstr)
 
-			topic.last_count = topic.count;
 
 		logging.debug ("updated entries " + str(updated_entries))
 
@@ -563,6 +648,10 @@ class MyApp:
 		# self.treeview.set_model (model)
 
 		main.infolabel6.set_markup('<span foreground="green">Active topics: ' + str(active_topics) + '</span>')
+		if main.matchprog is not None:
+			main.infolabel7.set_markup('<span foreground="red">Excluded: ' + str(main.excluded/5) + '</span>' + ' <span foreground="green">Shown: ' + str(len(main.shown)) + '</span>')
+		else:
+			self.infolabel7.set_text("")
 		# logging.debug ("completed_cb - exit")
 
 	def dump(self):
@@ -610,6 +699,24 @@ class Handler:
 	def on_gtk_about_activate(self, menuitem, data=None):
 		self.response = main.aboutdialog.run()
 		main.aboutdialog.hide()
+
+	def on_gtk_entry2_changed(self, item, data=None):
+		# logging.debug ("on_gtk_entry2_changed " + item.get_text())
+		if item.get_text() == '':
+			main.matchprog = None
+		else:
+			try:
+				main.matchprog = re.compile(item.get_text())
+			except:
+				main.matchprog = None
+
+		# force an update to all topics
+		main.shown.clear()
+		for topic in main.topics:
+			entry = main.topics[topic]
+			if entry.number not in main.changed:
+				main.changed[entry.number] = topic
+		return
 
 ###########################################################################
 if __name__ == "__main__":
